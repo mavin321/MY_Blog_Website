3bf0c273ff50c94b6cec51926b7314db39f0bef0
mavin321 <mavinpeteromondi@gmail.com>
2025-02-27 08:31:24 +0300

    version 3

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Post
+import markdown
+from django.utils.safestring import mark_safe
 
 def index(request):
     posts=Post.objects.all()
@@ -16,6 +18,7 @@ def featured(request):
 
 def details(request, pk):
     posts=Post.objects.get(id=pk)
+    posts.content = mark_safe(markdown.markdown(posts.content, extensions=["fenced_code", "tables", "toc"]))
     return render(request, 'blog-details.html', {'posts': posts})
 
 
